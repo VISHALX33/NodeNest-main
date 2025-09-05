2e3db04fe5f5439699c82cb802117bf448fa5654
Mohit Dad(Maheshwari) <123006118+mohitgit-hub@users.noreply.github.com>
2025-09-05 19:38:45 +0530

    URL Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     host: true, // so Vite binds to 0.0.0.0
 allowedHosts: [
-      '1abcbd72f82e.ngrok-free.app',
-      // 'notenests.netlify.app', // replace with your actual ngrok URL
+      // '1abcbd72f82e.ngrok-free.app',
+       'notenests.netlify.app', // replace with your actual ngrok URL
     ],
   },
 })    
